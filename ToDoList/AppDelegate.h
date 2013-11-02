68b34b3b83f09a4809ddeea18ec309486c44338a
Lucas Eager Leavitt <lucaseagerleavitt@Lucass-MacBook.local>
2013-11-02 13:13:27 -0400

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Lucas Eager Leavitt on 11/2/13.
+//  Copyright (c) 2013 Lucas Eager Leavitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Lucas Eager Leavitt on 11/2/13.
+//  Copyright (c) 2013 Lucas Eager Leavitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
